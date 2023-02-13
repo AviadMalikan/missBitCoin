aeffdf5b8a299705f34940fa6e4aa976052547e5
AviadMalikan <aviad879@gmail.com>
2023-02-13 17:01:39 +0200

    contact details re design

--- src/pages/contact-details.vue
+++ src/pages/contact-details.vue
@@ -1,26 +1,34 @@
 <template>
-    <div v-if="car" class="car-details">
-        <h2>{{car.vendor}}</h2>
-        <h3>{{car.speed}}</h3>
-        <RouterLink to="/contact">
-            <button>Back</button>
-        </RouterLink>
+  <div v-if="car" class="car-details">
+    <div class="contact-img">
+      <img :src="`https://robohash.org/${car.name}?set=set5`" alt="" />
     </div>
+    <article class="contact-details">
+      <h2>{{ car.name }}</h2>
+      <h4>Phone: {{ car.phone }}</h4>
+      <h4>Email: {{ car.email }}</h4>
+    <RouterLink to="/contact">
+      <button class="back-btn">Back</button>
+    </RouterLink>
+    </article>
+  </div>
 </template>
 
 <script>
-import { contactService } from '@/services/contact.service.js'
+import { contactService } from "@/services/contact.service.js";
 export default {
-    data() {
-        return {
-            car: null,
-        }
-    },
-    async created(){
-        const carId = this.$route.params._id
-        this.car = await contactService.get(carId)
-    }
-}
+  data() {
+    return {
+      car: null,
+    };
+  },
+  async created() {
+    const carId = this.$route.params._id;
+    console.log("carId: ", carId);
+
+    this.car = await contactService.get(carId);
+  },
+};
 </script>
 
 <style lang="scss"></style>
